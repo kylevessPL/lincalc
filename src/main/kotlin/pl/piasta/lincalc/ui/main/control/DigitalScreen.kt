d6ecc731251fa24b4038f866811025c275e4fdde
Kacper Piasta <kacperpiasta@gmail.com>
2023-03-20 22:45:23 +0100

    improve digital screen text size handling
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- src/main/kotlin/pl/piasta/lincalc/ui/main/control/DigitalScreen.kt
+++ src/main/kotlin/pl/piasta/lincalc/ui/main/control/DigitalScreen.kt
@@ -1,17 +1,66 @@
 package pl.piasta.lincalc.ui.main.control
 
-import javafx.scene.Cursor.HAND
+import javafx.beans.value.ChangeListener
+import javafx.scene.Cursor.DEFAULT
 import javafx.scene.control.TextField
+import javafx.scene.text.Font
+import javafx.scene.text.Text
+import pl.piasta.lincalc.common.Constants.DIGITAL_FONT
+import pl.piasta.lincalc.common.Constants.FONT_ASSETS
+import pl.piasta.lincalc.common.EMPTY
+import pl.piasta.lincalc.common.ZERO
+import pl.piasta.lincalc.common.toFont
 import pl.piasta.lincalc.ui.main.Styles.Companion.digitalScreen
 import tornadofx.*
 
 class DigitalScreen : TextField() {
+    private companion object {
+        const val FONT_SIZE_MIN = 25.0
+        const val TEXT_FIELD_PADDING = 20.0
+        val digitalFont = "$FONT_ASSETS/$DIGITAL_FONT".toFont(64.0)
+    }
+
     init {
         isEditable = false
-        cursor = HAND
+        font = digitalFont
+        cursor = DEFAULT
         selectedTextProperty().addListener { _, _, _ ->
             deselect()
         }
+        textProperty().addListener { _, oldValue, newValue ->
+            oldValue.takeUnless { it == String.EMPTY }?.let {
+                runAsync(true) {
+                    calculateFont(newValue)
+                } ui {
+                    font = calculateFont(newValue)
+                    positionCaret(text.length)
+                }
+            }
+        }
+        ChangeListener<Number> { _, oldValue, _ ->
+            oldValue.takeUnless { it == Double.ZERO }?.let {
+                runAsync(true) {
+                    calculateFont(text)
+                } ui {
+                    font = it
+                    positionCaret(text.length)
+                }
+            }
+        }.run {
+            widthProperty().addListener(this)
+            heightProperty().addListener(this)
+        }
         addClass(digitalScreen)
+    }
+
+    private fun calculateFont(newValue: String?): Font? {
+        val tmpText = Text(newValue)
+        tmpText.font = digitalFont
+        val textWidth = tmpText.layoutBounds.width
+        val maxWidth = width - 2 * TEXT_FIELD_PADDING
+        return if (textWidth > maxWidth) {
+            val calculatedSize = digitalFont!!.size * maxWidth / textWidth
+            Font.font(digitalFont.family, calculatedSize.coerceAtLeast(FONT_SIZE_MIN))
+        } else digitalFont
     }
 }
